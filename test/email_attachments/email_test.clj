da067fe54a89dd588885a702cb5511dbaeb46f86
davidvujic <david@vujic.se>
2021-02-12 18:16:27 +0100

    Add query namespace, with features for querying file names and searching by file name

--- test/email_attachments/email_test.clj
+++ test/email_attachments/email_test.clj
@@ -1,15 +1,37 @@
 (ns email-attachments.email-test
   (:require [clojure.java.io :as io]
-            [email-attachments.email :as email]))
+            [email-attachments.email :as email]
+            [email-attachments.query :as query]))
 
 (comment
-  (->> "emails/excel.eml"
+  (->> "emails/example2.eml"
        io/input-stream
        email/content-types))
+
+(comment
+  (->> "emails/example4.eml"
+       io/input-stream
+       email/content-types
+       first
+       email/content-stream))
 
 (comment
   (->> "emails/excel.eml"
        io/input-stream
        email/content-types
        first
-       email/content-stream))
+       query/filename))
+
+(comment
+  (->> "emails/example2.eml"
+       io/input-stream
+       email/content-types
+       query/filenames))
+
+(comment
+  (def content-types (->> "emails/example4.eml"
+                          io/input-stream
+                          email/content-types))
+
+  (def filename "email-exempel.csv")
+  (query/find-in content-types filename))
